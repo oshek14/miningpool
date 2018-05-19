2e58874cfd3d187060dddca97834e7cdc26103b0
novaknole <gioskofield@gmail.com>
2018-05-19 17:47:24 +0400

    some changes

--- libs/updateStats.js
+++ libs/updateStats.js
@@ -6,7 +6,7 @@ module.exports = function(logger){
     var portalConfig = JSON.parse(process.env.portalConfig);
     var poolConfigs = JSON.parse(process.env.pools);
 
-    console.log(portalConfig);
+    
 
 
 
