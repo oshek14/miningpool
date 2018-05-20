cc6e4a7dcf88c2f044a33b7602bf7f779876361f
novaknole <gioskofield@gmail.com>
2018-05-20 14:13:35 +0400

    t

--- libs/updateStats.js
+++ libs/updateStats.js
@@ -1,14 +1,14 @@
 
+const configHelper = require('../my_website/helpers/config_helper');
 
 module.exports = function(logger){
-
     
     var portalConfig = JSON.parse(process.env.portalConfig);
     var poolConfigs = JSON.parse(process.env.pools);
+
+    var coinStats = configHelper.getCoinStats(poolConfigs);
     
-    
-    console.log("here it comes");
-    
+    console.log(coinStats);
 
 
 
--- my_website/helpers/config_helper.js
+++ my_website/helpers/config_helper.js
@@ -24,5 +24,76 @@ module.exports = {
     },
     getCoinConfig : function(coin){
         return JSON.parse(JSON.minify(fs.readFileSync(coinDir+coin,{encoding:'utf8'})));
+    },
+    
+    getCoinStats:function(pool_configs){
+        var poolConfigsData = {};
+        var coinStats = {};
+        var redisClient = redis.createClient("6777",'165.227.143.126');
+        var redisCommands = [];
+        var commandsPerCoin = 5;
+        var data = pool_configs;
+        for(var i=0;i<data.length;i++){
+            var coin_name = data[i].coin.split('.')[0];
+            var coinConfig = configHelper.getCoinConfig(data[i].coin);
+            poolConfigsData[coin_name] = {
+                coinConfigs:coinConfig,
+                poolCoinConfig:data[i],   
+            };
+            var tabStatsCommand = [
+                ['zrangebyscore', coin_name+':hashrate', (Date.now() -  configHelper.hashRateStatTime*1000)/1000, '+inf'],
+                ['hgetall', coin_name+':stats'],
+                ['scard', coin_name+':blocksPending'],
+                ['scard', coin_name+':blocksConfirmed'],
+                ['scard', coin_name+':blocksKicked']
+            ];
+            redisCommands = redisCommands.concat(tabStatsCommand);
+        }
+        redisClient.multi(redisCommands).exec(function(err,res){
+            if(err){
+                //needs implementation 
+                return;
+            }else{
+                for(var i=0;i<Object.keys(poolConfigsData).length;i++){
+                    var coin_name =  Object.keys(poolConfigsData)[i];
+                    var algorithm = poolConfigsData[coin_name].coinConfigs.algorithm;
+                    var hashratesPerCoin = res[i*commandsPerCoin];
+                    var workersSet = new Set;
+                    var shares = 0;
+                    hashratesPerCoin.forEach(minerRate => {
+                        var miner_address = minerRate.split(":")[1];
+                        var difficulty = parseFloat(minerRate.split(":")[0]);
+                        if(difficulty > 0) shares+=difficulty;
+                        workersSet.add(miner_address);
+                    });
+
+                    var workersCount = workersSet.size;
+                    delete workersSet;
+
+                    var shareMultiplier = Math.pow(2, 32) / algos[algorithm].multiplier;
+                    var hashrate = shareMultiplier * shares / configHelper.hashRateStatTime;
+
+                    coinStats[coin_name] = {
+                        blocks:{
+                            pendingCount:res[i*commandsPerCoin+2],
+                            confirmedCount: res[i*commandsPerCoin+3],
+                            orphanedOrKicked:res[i*commandsPerCoin+4],
+                        },
+                        stats:{
+                            validShares:res[i*commandsPerCoin+1] ? (res[i*commandsPerCoin+1].validShares || 0) :0,
+                            invalidShares:res[i*commandsPerCoin+1] ? (res[i*commandsPerCoin+1].invalidShares || 0) :0,
+                            validBlocks:res[i*commandsPerCoin+1] ? (res[i*commandsPerCoin+1].validBlocks || 0) :0,
+                            totalPaid:res[i*commandsPerCoin+1] ? (res[i*commandsPerCoin+1].totalPaid || 0) :0,
+                        },
+                        hashrate:hashrate,
+                        algorithm:algorithm,
+                        workersCount:workersCount,
+                    }
+                    
+                }
+                return coinStats;
+            }
+        })
+        
     }
 }
