b113c89df36a81843bde02ca2bb687c7eb304624
novaknole <gioskofield@gmail.com>
2018-05-21 20:08:55 +0400

    t

--- my_website/helpers/config_helper.js
+++ my_website/helpers/config_helper.js
@@ -195,7 +195,7 @@ module.exports = {
                 for (var worker in workers) {
                     workers[worker].hashrateString = module.exports.getReadableHashRateString( shareMultiplier * workers[worker].shares / (time_stats / 1000));
                 }
-                console.log(workers);
+                
                 workerStats[coin_name] = workers;
             }
             
