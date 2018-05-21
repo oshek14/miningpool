b68f49d09e3061b061d68c73c2d04ef45de476b1
novaknole <gioskofield@gmail.com>
2018-05-22 01:07:16 +0400

    t

--- crypto_test.js
+++ crypto_test.js
@@ -6,8 +6,8 @@ var redisClient = redis.createClient("6777", "165.227.143.126");
 var dateNow = Date.now();
 var minerPaid = [1,3,dateNow];
 
-console.log(dateNow);
-
+var date = new Date(dateNow);
+console.log(date.getHours(), " ",date.getMinutes());
 
 
 // redisClient.multi([
