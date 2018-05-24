866709335b35cad5f92fa5115744a1b89ab615e1
novaknole <gioskofield@gmail.com>
2018-05-24 13:22:20 +0400

    T

--- crypto_test.js
+++ crypto_test.js
@@ -10,12 +10,8 @@ const isKb = true       // or Mb
 const m = new jm({isPrint, isMs, isKb})
 
 
-var c = [];
-for(var j=0;j<300000;j++){
-    c.push(['zadd','bitcoin:stat:workers:hourly:'+j,Date.now()/1000,'1']);
-    
-}
-redisClient.multi(c).exec(function(err,res){
+
+
     redisClient.multi([
         ['keys','bitcoin:stat:workers:hourly:*'],
         ['zrangebyscore','bitcoin:stat:global:hourly','-inf','+inf']
@@ -24,26 +20,36 @@ redisClient.multi(c).exec(function(err,res){
         if(err){
             //TODO
         }else{
-            console.log("movida");
+            
             var globalHourly = res[1];
             var workersKeys = res[0];
+
             var workersResultKeys={};
             var getCommandsQuery=[];
             for(var i=0;i<workersKeys.length;i++){
-                getCommandsQuery.push(['zrangebyscore',workersKeys[i],'-inf','+inf']);
+                workersResultKeys[i] = workersKeys[i];
+                console.log(workersKeys[i])
+                getCommandsQuery.push(['zrevrangebyscore',workersKeys[i],'+inf','-inf', 'limit', 0, 24]);
             }
-            console.log("morcha");
+
             redisClient.multi(getCommandsQuery).exec(function(err,res){
+                var workersData = {};
                 for(var j=0;j<res.length;j++){
+                    var workerData = {}
                     var data = res[j];
-                    
+                    var worker = workersResultKeys[j].split(":")[4];  
+                    workersData[worker] = data
                 }
-                console.log("good");
-                const meter = m.stop()
+                console.log(workersData)
+                // for (var j = 0; j < workersData.length; j++) {
+                //     var workerName = workersData[j]
+                //     var data = 
+                    
+                // }
             })
         }
     });
-})
+
 
 
 
