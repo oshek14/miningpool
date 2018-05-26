621040d7ef1668d58ab31ff5b95897fb04e340fc
novaknole <gioskofield@gmail.com>
2018-05-26 21:10:52 +0400

    T

--- my_website/functions/coins.js
+++ my_website/functions/coins.js
@@ -1,8 +1,7 @@
 
 var redis = require('redis')
+var configHelper = require('../helpers/config_helper')
 module.exports = {
-
-
     getStatsForEachCoin:function(pool_configs,callback){
         var poolConfigsData = {};
         var coinStats = {};
@@ -76,23 +75,30 @@ module.exports = {
     },
 
     getLastStats: function(coin, algo, callback) {
-        var redisClient = redis.createClient("6777",'165.227.143.126');
-        redisComands = [
-            ['zrevrangebyscore', coin + ':stat:global:tenMinutes', '+inf', (Date.now() - 10*60*1000)/1000, 'limit', 0, 1],
-            ['zrevrangebyscore', coin + ':stat:global:hourly', '+inf', '-inf', 'limit', 0, 1],
-            ['zrevrangebyscore', coin + ':stat:global:daily', '+inf', '-inf', 'limit', 0, 1],
-            ['scard', coin + ':blocksConfirmed'],
-            ['scard', coin + ':blocksPending'],
-            ['scard', coin + ':blocksOrphaned'],
-            ['scard', coin + ':blocksKicked'],
-            ['scard', coin + ':existingWorkers'],
-        ] 
-        redisClient.multi(redisComands).exec(function(err, res) {
-            if (err) {
-
-            } else {
-                callback(res)
-            }
+        
+        configHelper.getPoolConfigs(function(data) {
+            console.log(data);
         })
+        
+        // var redisClient = redis.createClient("6777",'165.227.143.126');
+        // redisComands = [
+        //     ['zrevrangebyscore', coin + ':stat:global:tenMinutes', '+inf', (Date.now() - 10*60*1000)/1000, 'limit', 0, 1],
+        //     ['zrevrangebyscore', coin + ':stat:global:hourly', '+inf', '-inf', 'limit', 0, 1],
+        //     ['zrevrangebyscore', coin + ':stat:global:daily', '+inf', '-inf', 'limit', 0, 1],
+        //     ['scard', coin + ':blocksConfirmed'],
+        //     ['scard', coin + ':blocksPending'],
+        //     ['scard', coin + ':blocksOrphaned'],
+        //     ['scard', coin + ':blocksKicked'],
+        //     ['scard', coin + ':existingWorkers'],
+        // ] 
+
+        // //pool balance how muhc paid how much we need to pay.
+        // redisClient.multi(redisComands).exec(function(err, res) {
+        //     if (err) {
+
+        //     } else {
+        //         callback(res)
+        //     }
+        // })
     }
 }
