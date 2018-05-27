91c1f07437286283364abc4faec8bba7c66e5327
novaknole <gioskofield@gmail.com>
2018-05-27 18:43:05 +0400

    T'

--- my_website/functions/workers.js
+++ my_website/functions/workers.js
@@ -6,13 +6,13 @@ module.exports = {
         var redisCommands = [];
         var date  = Date.now();
         var redisClient = redis.createClient("6777",'165.227.143.126');
-        redisClient.smembers(coin_name+':existingWorkers',function(err,res){
-            if(err) callback(500);
+        redisClient.smembers(coin_name+':existingWorkers',function(err,workersKeys){
+            if(workersKeys) callback(500);
             else
                 for(var j=0;j<res.length;j++){
                     redisCommands.push(['zrevrangebyscore',coin_name+':stat:workers:tenMinutes:'+res[j],'+inf',(date-10*60*1000)/1000,'limit',0,1]);
                 }
-                redisClient.multi(redisCommands).exec(function(err,res){
+                redisClient.multi(redisCommands).exec(function(err,workersValues){
                     if(err) callback(500);
                     else callback({workersKeys:workersKeys,workersValues:workersValues});
                 })
