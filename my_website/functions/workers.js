53bbd1040a4919e99c8c7ae66e3897d03179607e
novaknole <gioskofield@gmail.com>
2018-05-27 18:46:51 +0400

    T

--- my_website/functions/workers.js
+++ my_website/functions/workers.js
@@ -9,7 +9,7 @@ module.exports = {
         redisClient.smembers(coin_name+':existingWorkers',function(error,workersKeys){
             if(error) callback(500);
             else
-                for(var j=0;j<res.length;j++){
+                for(var j=0;j<workersKeys.length;j++){
                     redisCommands.push(['zrevrangebyscore',coin_name+':stat:workers:tenMinutes:'+res[j],'+inf',(date-10*60*1000)/1000,'limit',0,1]);
                 }
                 redisClient.multi(redisCommands).exec(function(err,workersValues){
