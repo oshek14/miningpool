a1228eaa0b539e359cc75762f2720d17898a54cd
novaknole <gioskofield@gmail.com>
2018-05-29 18:28:23 +0400

    t

--- my_website/functions/users.js
+++ my_website/functions/users.js
@@ -32,8 +32,6 @@ module.exports = {
                         finalData['result'] = res;
                         callback(finalData);
                     }
-                        
-
                 })
                 
         })
