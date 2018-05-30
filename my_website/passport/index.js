4fae2e7f29d67ddca63101b41697d9d9da86d497
novaknole <nikakhurashvili@gmail.com>
2018-05-31 00:33:07 +0400

    ss

--- my_website/passport/index.js
+++ my_website/passport/index.js
@@ -2,11 +2,13 @@ var JwtStrategy = require('passport-jwt').Strategy
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 module.exports = function(passport) {
+    console.log('aqaa')
     console.log(passport)
     var opts = {};
     opts.jwtFromRequest = ExtractJwt.fromHeader();
     opts.secretOrKey = "asdasgasgasgfausyuiasfhiausfi";
     passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-    console.log(jwt_payload)
+        console.log('aqaa2')
+        console.log(jwt_payload)
     }));
 };
