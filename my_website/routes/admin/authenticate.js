e681f3402cbd44a31d5321b7a82f8e621154213e
novaknole <nikakhurashvili@gmail.com>
2018-05-31 01:49:51 +0400

    s

--- my_website/routes/admin/authenticate.js
+++ my_website/routes/admin/authenticate.js
@@ -6,28 +6,28 @@ var jwt = require('jsonwebtoken');
 require('../../passport')(passport);
 var secret = require('../../passport/constants')
 
-router.post('/signin', passport.authenticate('jwt', { session: false}), function(req, res) {
+router.post('/signin', function(req, res) {
     console.log('sasas')
-    // var redisClient = redis.createClient("6777", "165.227.143.126");
+    var redisClient = redis.createClient("6777", "165.227.143.126");
     var email = req.body.email
     var password = req.body.password
     console.log(email)
     console.log(password)
-    //  redisClient.hget('administrators', email , function(err, result) {
-    //     if (err) {
-    //         res.send({status: 500})
-    //     } else if (result) {
-    //         var parsedRes = JSON.parse(result)
-    //         if (parsedRes.password === password) {
-    //             var token = jwt.sign(parsedRes, secret);
-    //             res.send({status: 200, token: token})
-    //         } else {
-    //             res.send({status: 500})
-    //         }
-    //     } else {
-    //         res.send({status: 500})
-    //     }
-    //  })
+     redisClient.hget('administrators', email , function(err, result) {
+        if (err) {
+            res.send({status: 500})
+        } else if (result) {
+            var parsedRes = JSON.parse(result)
+            if (parsedRes.password === password) {
+                var token = jwt.sign(parsedRes, secret);
+                res.send({status: 200, token: token})
+            } else {
+                res.send({status: 500})
+            }
+        } else {
+            res.send({status: 500})
+        }
+     })
 })
 
 module.exports = router;
