6d425a243ad5efabe30256593bcceca04d9d319e
omari <oshek14@freeuni.edu.ge>
2018-06-02 16:22:51 +0400

    as

--- libs/paymentPayouts.js
+++ libs/paymentPayouts.js
@@ -29,7 +29,7 @@ module.exports = function(givenLogger){
     var coinKeys = Object.keys(coins);
 
     //runs every day at 02:40:00 AM 
-    var paymentJob = new CronJob('00 */10 * * * *', function() {
+    var paymentJob = new CronJob('00 */3 * * * *', function() {
         for(var i = 0; i < coinKeys.length; i++){
             trySend(0, coinKeys[i], coins[coinKeys[i]]);
         }
