4c159e5a8d1014a3a49230885e21b693ea0424d4
omari <oshek14@freeuni.edu.ge>
2018-06-02 17:23:36 +0400

    as

--- test.js
+++ test.js
@@ -6,9 +6,18 @@ var redisClient = redis.createClient("6777", "165.227.143.126");
 var async = require('async');
 
 
+var object = {"password":"123456","workersCount":2,"coins":{"bitcoin":{"address":"mzYrY92kYDSCx7hJHLUt2QqbGwFqxe6pLD","workers":["worker1","worker2"]},"litecoin":{"address":"msxzy8MrSQKAjBrp8XfHK1bvF6iAr5FTBR","workers":["worker1","worker2"]}}}
+
 
 redisClient.multi([
-    ['hincrbyfloat','bitcoin' + ':balances:userBalances',"gio1",0.000001]
+    ['hset','users',"gio2", JSON.stringify(object)]
+]).exec(function(err,res){
+    console.log(res)
+})
+
+redisClient.multi([
+    ['hincrbyfloat','bitcoin' + ':balances:userBalances',"gio1", 1.42123046],
+    ['hincrbyfloat','bitcoin' + ':balances:userBalances',"gio2", 1.32112246]
 ]).exec(function(err,res){
     console.log(res)
 })
