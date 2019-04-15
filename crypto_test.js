891574c3a80a5275001506b19b12d99a546f20dd
oshek14 <oshek14@freeuni.edu.ge>
2019-04-15 19:12:31 +0400

    seed

--- crypto_test.js
+++ crypto_test.js
@@ -365,21 +365,21 @@ function init(coin,howManyUsers,workersPerUser,firstIndex,address){
     
         if(!err){
             console.log("modis");
-            // putCoinStat(coin);
-            // put10MinutesDataForGlobal(coin);
-            // put10MinutesDataForWorkers(coin,howManyUsers,workersPerUser,firstIndex);
-            // put24HoursDataForGlobal(coin);
-            // put24HoursDataForWorkers(coin,howManyUsers,workersPerUser,firstIndex);
-            // put30DaysDataForGlobal(coin);
-            // put30DaysDataForWorkers(coin,howManyUsers,workersPerUser,firstIndex);
-             //putExistingWorkers(coin,howManyUsers,workersPerUser,firstIndex);
-            // putUserBalances(coin,howManyUsers,firstIndex);
-            // workersValidInvalid(coin,howManyUsers,workersPerUser,firstIndex);
-            // putUserPayouts(coin,howManyUsers,firstIndex,address);
-            // putUserTotalPaid(coin,howManyUsers,firstIndex,address);
-            //putUsers(coin,howManyUsers,workersPerUser,firstIndex);
-            // putTransactions(coin);
-            //putBlocksInfo(coin);
+            putCoinStat(coin);
+            put10MinutesDataForGlobal(coin);
+            put10MinutesDataForWorkers(coin,howManyUsers,workersPerUser,firstIndex);
+            put24HoursDataForGlobal(coin);
+            put24HoursDataForWorkers(coin,howManyUsers,workersPerUser,firstIndex);
+            put30DaysDataForGlobal(coin);
+            put30DaysDataForWorkers(coin,howManyUsers,workersPerUser,firstIndex);
+             putExistingWorkers(coin,howManyUsers,workersPerUser,firstIndex);
+            putUserBalances(coin,howManyUsers,firstIndex);
+            workersValidInvalid(coin,howManyUsers,workersPerUser,firstIndex);
+            putUserPayouts(coin,howManyUsers,firstIndex,address);
+            putUserTotalPaid(coin,howManyUsers,firstIndex,address);
+            putUsers(coin,howManyUsers,workersPerUser,firstIndex);
+            putTransactions(coin);
+            putBlocksInfo(coin);
             console.log("DONE");
         }else{
             console.log(err);
@@ -388,41 +388,41 @@ function init(coin,howManyUsers,workersPerUser,firstIndex,address){
     
 }
 
-// var tenMinutesJob = new CronJob('05 */3 * * * *', function() {
-//     console.log("asdasdasdasdasdasdasdasdasdasd");
-//     //saveStatsEveryTenMinutes(portalConfig,poolConfigs,redisClients);
-// }, null, true, null);
+var tenMinutesJob = new CronJob('05 */3 * * * *', function() {
+    console.log("asdasdasdasdasdasdasdasdasdasd");
+    saveStatsEveryTenMinutes(portalConfig,poolConfigs,redisClients);
+}, null, true, null);
 
 
 
 
-//init('bitcoin',1,2,"gio","niceoneaddress");
-// redisClient.zscore('bitcoin'+':blocks:info',8888,function(error,result){
-//     console.log(result);
+init('bitcoin',1,2,"gio","niceoneaddress");
+redisClient.zscore('bitcoin'+':blocks:info',8888,function(error,result){
+    console.log(result);
 
-// })
-// init('litecoin',1,2,"nika","addresslitecoin");
-// var redisCommands = [];
-// redisCommands.push(['zadd','bitcoin'+':blocksConfirmedInformation', 'NX',Date.now()/1000,125]);
-// // redisClient.multi(redisCommands).exec(function(err,res){
-// //     console.log(err);
-// //     console.log(res);
-// // })
-// redisCommands.push(['hget','bitcoin:blocksConfirmedInformation',"gio1"]);
-// redisClient.hget('bitcoin:blocksConfirmedInformation','gio1',function(err,res){
-//     console.log(err);
-//     console.log(res);
-// })
-// redisClient.zrangebyscore('bitcoin'+':blocksConfirmedInformation','-inf',2386,function(err,res){
-//    console.log(err);
-//     console.log(res);
-// })
-// var command = [];
-// command.push(['hset',
-//     'bitcoin:blocks:confirmedInfo',
-//     13204293,
-//     JSON.stringify({"startDate":"1527798697","endDate":1527798971})]);
-// redisClient.multi(command).exec(function(err,res){
-// console.log(err);
-// console.log(res);
-// })
+})
+init('litecoin',1,2,"nika","addresslitecoin");
+var redisCommands = [];
+redisCommands.push(['zadd','bitcoin'+':blocksConfirmedInformation', 'NX',Date.now()/1000,125]);
+redisClient.multi(redisCommands).exec(function(err,res){
+    console.log(err);
+    console.log(res);
+})
+redisCommands.push(['hget','bitcoin:blocksConfirmedInformation',"gio1"]);
+redisClient.hget('bitcoin:blocksConfirmedInformation','gio1',function(err,res){
+    console.log(err);
+    console.log(res);
+})
+redisClient.zrangebyscore('bitcoin'+':blocksConfirmedInformation','-inf',2386,function(err,res){
+   console.log(err);
+    console.log(res);
+})
+var command = [];
+command.push(['hset',
+    'bitcoin:blocks:confirmedInfo',
+    13204293,
+    JSON.stringify({"startDate":"1527798697","endDate":1527798971})]);
+redisClient.multi(command).exec(function(err,res){
+console.log(err);
+console.log(res);
+})
